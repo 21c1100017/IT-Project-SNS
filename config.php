e9045ef7c5becd4b3fd98127ff93b92abfefad75
吉田 健人 <21c1100017yk@edu.tech.ac.jp>
2021-12-21 15:02:29 +0900

    update

--- config.php
+++ config.php
@@ -9,9 +9,9 @@ if(!defined("allow_access_config")){
 
 //ini_set('display_errors', 0);
 session_start();
-define("allow_access_db_connect", true);
-require_once(__DIR__ . "/db_connect.php");
 define("allow_access_functions", true);
 require_once(__DIR__ . "/functions.php");
+define("allow_access_db_connect", true);
+require_once(__DIR__ . "/db_connect.php");
 
 $title = "簡単なSNS";
